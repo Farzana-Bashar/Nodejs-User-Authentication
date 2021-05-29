7b9d080153f691f165611fef640e1fdf1440cb18
Farzana Bashar <farzana@Farzanas-MacBook-Pro.local>
2021-05-29 15:08:24 +0600

    hasing password using bcrypt

---
+++ server.js
@@ -0,0 +1,31 @@
+const express= require('express');
+const app= express();
+const bcrypt= require('bcrypt');
+
+app.use(express.json());
+
+const users=[];
+
+app.get('/users', (req,res)=>{
+    res.json(users);
+});
+
+app.post('/users', async (req,res)=>{
+     
+
+    try{
+        const hashedPassword =  await bcrypt.hash(req.body.password, 10);
+        const user= { name: req.body.name, password: hashedPassword };
+        users.push(user);
+        res.status(201),send();
+    } catch(error){
+        res.status(401).send(error)
+    }
+
+});
+
+app.post('/users/login', async (req,res)=>{
+    
+})
+
+app.listen(3000);
